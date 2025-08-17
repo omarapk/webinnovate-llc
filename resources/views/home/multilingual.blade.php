c66223b3903761dea4f19f34e5c51149f724ac8b
abdellahe hammani <abdellahe.7ammani@gmail.com>
2025-08-17 16:26:51 +0200

    v10

--- resources/views/home/multilingual.blade.php
+++ resources/views/home/multilingual.blade.php
@@ -2609,6 +2609,153 @@
             </div>
         </div>
     </div>
+        <div class="rbt-breadcrumb-default ptb--100 ptb_md--50 ptb_sm--30 bg-gradient-1">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="breadcrumb-inner text-center">
+                        <h2 class="title">Faqs</h2>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+<div class="rbt-accordion-area accordion-style-1 bg-color-white rbt-section-gap">
+        <div class="container">
+            <div class="row g-5">
+                <div class="col-lg-6">
+                    <div class="rbt-accordion-style accordion">
+
+                        <div class="rbt-accordion-style rbt-accordion-04 accordion">
+                            <div class="accordion" id="accordionExamplec3">
+                                <div class="accordion-item card">
+                                    <h2 class="accordion-header card-header" id="headingThree1">
+                                        <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree1" aria-expanded="true" aria-controls="collapseThree1">
+                                            What is Histudy ? How does it work?
+                                        </button>
+                                    </h2>
+                                    <div id="collapseThree1" class="accordion-collapse collapse show" aria-labelledby="headingThree1" data-bs-parent="#accordionExamplec3">
+                                        <div class="accordion-body card-body">
+                                            You can run Histudy easily. Any School, University, College can be use this histudy education template for their educational purpose. A university can be run their online leaning management system by histudy education template.
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="accordion-item card">
+                                    <h2 class="accordion-header card-header" id="headingThree2">
+                                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree2" aria-expanded="false" aria-controls="collapseThree2">
+                                            How can I get the customer support?
+                                        </button>
+                                    </h2>
+                                    <div id="collapseThree2" class="accordion-collapse collapse" aria-labelledby="headingThree2" data-bs-parent="#accordionExamplec3">
+                                        <div class="accordion-body card-body">
+                                            After purchasing the product need you any support you can be share with
+                                            us with sending mail to pixcelsthemes@gmail.com.
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="accordion-item card">
+                                    <h2 class="accordion-header card-header" id="headingThree3">
+                                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree3" aria-expanded="false" aria-controls="collapseThree3">
+                                            Can I get update regularly and For how long do I get updates?
+                                        </button>
+                                    </h2>
+                                    <div id="collapseThree3" class="accordion-collapse collapse" aria-labelledby="headingThree3" data-bs-parent="#accordionExamplec3">
+                                        <div class="accordion-body card-body">
+                                            Yes, We will get update the Histudy. And you can get it any time. Next
+                                            time we will comes with more feature. You can be get update for
+                                            unlimited times. Our dedicated team works for update.
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="accordion-item card">
+                                    <h2 class="accordion-header card-header" id="headingThree4">
+                                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree4" aria-expanded="false" aria-controls="collapseThree4">
+                                            15 Things To Know About Education?
+                                        </button>
+                                    </h2>
+                                    <div id="collapseThree4" class="accordion-collapse collapse" aria-labelledby="headingThree4" data-bs-parent="#accordionExamplec3">
+                                        <div class="accordion-body card-body">
+                                            If you're looking for random paragraphs, you've come to the right place. When a random word or a random sentence isn't quite enough, the next logical step is to find a random paragraph.
+                                        </div>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-6">
+                    <div class="rbt-accordion-style accordion">
+                        <div class="rbt-accordion-style rbt-accordion-04 accordion">
+                            <div class="accordion" id="faqs-accordionExamplec3">
+                                <div class="accordion-item card">
+                                    <h2 class="accordion-header card-header" id="faqs-headingThree1">
+                                        <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#faqs-collapseThree1" aria-expanded="true" aria-controls="faqs-collapseThree1">
+                                            What is Histudy ? How does it work?
+                                        </button>
+                                    </h2>
+
+                                    <div id="faqs-collapseThree1" class="accordion-collapse collapse show" aria-labelledby="faqs-headingThree1" data-bs-parent="#faqs-accordionExamplec3">
+                                        <div class="accordion-body card-body">
+                                            You can run Histudy easily. Any School, University, College can be use this histudy education template for their educational purpose. A university can be run their online leaning management system by histudy education template.
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="accordion-item card">
+                                    <h2 class="accordion-header card-header" id="faqs-headingThree2">
+                                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#faqs-collapseThree2" aria-expanded="false" aria-controls="faqs-collapseThree2">
+                                            7 Facts About Education That Will Blow
+                                        </button>
+                                    </h2>
+                                    <div id="faqs-collapseThree2" class="accordion-collapse collapse" aria-labelledby="faqs-headingThree2" data-bs-parent="#faqs-accordionExamplec3">
+                                        <div class="accordion-body card-body">
+                                            After purchasing the product need you any support you can be share with
+                                            us with sending mail to pixcelsthemes@gmail.com.
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="accordion-item card">
+                                    <h2 class="accordion-header card-header" id="faqs-headingThree3">
+                                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#faqs-collapseThree3" aria-expanded="false" aria-controls="faqs-collapseThree3">
+                                            10 Brilliant Ways To Advertise Education
+                                        </button>
+                                    </h2>
+                                    <div id="faqs-collapseThree3" class="accordion-collapse collapse" aria-labelledby="faqs-headingThree3" data-bs-parent="#faqs-accordionExamplec3">
+                                        <div class="accordion-body card-body">
+                                            Yes, We will get update the Histudy. And you can get it any time. Next
+                                            time we will comes with more feature. You can be get update for
+                                            unlimited times. Our dedicated team works for update.
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="accordion-item card">
+                                    <h2 class="accordion-header card-header" id="faqs-headingThree4">
+                                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#faqs-collapseThree4" aria-expanded="false" aria-controls="faqs-collapseThree4">
+                                            15 Common Myths About Education
+                                        </button>
+                                    </h2>
+                                    <div id="faqs-collapseThree4" class="accordion-collapse collapse" aria-labelledby="faqs-headingThree4" data-bs-parent="#faqs-accordionExamplec3">
+                                        <div class="accordion-body card-body">
+                                            If you're looking for random paragraphs, you've come to the right place. When a random word or a random sentence isn't quite enough, the next logical step is to find a random paragraph.
+                                        </div>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
     <div class="rbt-pricing-area bg-color-extra2 rbt-section-gap">
         <div class="container">
             <div class="row g-5 mb--60">
